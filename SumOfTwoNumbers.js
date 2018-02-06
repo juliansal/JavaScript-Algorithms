37c98ee505ab1c1fdbdcba7d2997d1bf52f28f71
Julio Salguero <julian@juliandesignstudio.com>
2018-02-06 00:04:32 -0800

    Update SumOfTwoNumbers.js

--- SumOfTwoNumbers.js
+++ SumOfTwoNumbers.js
@@ -3,22 +3,22 @@ var evalArray = [2, 0, 4, 6, 8, 9]; //values to evaluate
 var sum = 8; //the chosen value that must be added up to
 
 var loopTwoNumbers = function(evalArray) { 
-  for (var i = 0, j = 1; j < evalArray.length; j++) {
-    var hasSum = sumOfTwoNumbers(evalArray[i], evalArray[j], sum);
-    console.log(hasSum);
-    if (evalArray.length - 1 == j) {
-      j = i + 1;
-      i++;
-    }
-  }
+for (var i = 0, j = 1; j < evalArray.length; j++) {
+	var hasSum = sumOfTwoNumbers(evalArray[i], evalArray[j], sum);
+		console.log(hasSum);
+		if (evalArray.length - 1 == j) {
+			j = i + 1;
+			i++;
+		}
+	}
 };
 
 var sumOfTwoNumbers = function(first, second, sum) {
-  if (first + second == sum) {
-    return true;
-  } else {
-    return false;
-  }
+	if (first + second == sum) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 loopTwoNumbers(evalArray);
