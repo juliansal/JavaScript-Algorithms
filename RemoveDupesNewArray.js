a365d553555924baf5e9b2d7708970b9e3bf2a51
Julio Salguero <julian@juliandesignstudio.com>
2018-02-06 00:07:53 -0800

    Update RemoveDupesNewArray.js

--- RemoveDupesNewArray.js
+++ RemoveDupesNewArray.js
@@ -1,6 +1,7 @@
 //removes duplicate items in an array by making a new array
 var xArr = [10,20,40,40,100,10,20,40,80,0,60,60];
 var newArr = [];
+
 for(var i = 0; i < xArr.length; i++) {
 	if(newArr.includes(xArr[i], 0)) {
 		;
